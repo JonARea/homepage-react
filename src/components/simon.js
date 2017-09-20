00754c4d0c56e9b82d5a5f6aa614fe5f8791932e
JonARea <jonarea@gmail.com>
2017-09-20 05:06:37 -0400

    simon lights work and style looks right

--- src/components/simon.js
+++ src/components/simon.js
@@ -2,24 +2,49 @@ import React, { Component } from 'react'
 import $ from 'jquery'
 import '../styles/simon.css'
 
+const blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+const yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
+const greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
+const redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+
+
 class Simon extends Component {
+
   componentWillMount() {
     $('div.content-detail').css('background-image', 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2-vz_g3WI9RSUqhKIhd6KqFFpxt22PHYxJI6tJXRP1wNq2rTY")');
+
   }
 
   componentWillUnmount() {
     $('div.content-detail').css('background-image', 'none')
   }
+
+  lightButton(e) {
+    e.target.classList.add(e.target.id + '-lit')
+    e.target.id === 'blue-button' ? blueSound.play() :
+    e.target.id === 'yellow-button' ? yellowSound.play() :
+    e.target.id === 'green-button' ? greenSound.play() :
+    redSound.play()
+
+  }
+
+  unlightButton(e) {
+    e.target.classList.remove(e.target.id + '-lit')
+  }
+
   render() {
+
+
+
     return (
       <div id='simon-border'>
-        <div className='button' id='blue-button'>
+        <div className='button' id='blue-button' onMouseDown={this.lightButton} onMouseUp={this.unlightButton}>
         </div>
-        <div className='button' id='green-button'>
+        <div className='button' id='green-button' onMouseDown={this.lightButton} onMouseUp={this.unlightButton}>
         </div>
-        <div className='button' id='red-button'>
+        <div className='button' id='red-button' onMouseDown={this.lightButton} onMouseUp={this.unlightButton}>
         </div>
-        <div className='button' id='yellow-button'>
+        <div className='button' id='yellow-button' onMouseDown={this.lightButton} onMouseUp={this.unlightButton}>
         </div>
         <div id='center-border'>
           <div id='controls'>
@@ -38,15 +63,17 @@ class Simon extends Component {
             </div>
 
             <div id='on-off-holder'>
+              <p id='on'>ON</p>
               <div id='on-off-switch'>
                 <div id='on-off-toggle'>
                 </div>
               </div>
-              <p>ON OFF</p>
+              <p id='off'>OFF</p>
             </div>
 
             <div id='counter-holder'>
               <div id='counter'>
+                <p>12</p>
               </div>
               <p>COUNT</p>
             </div>
