53e3ff9b8f5c6db781d090c2d735a61683d60231
Jon Rea <jonarea@gmail.com>
2017-11-06 16:57:47 -0500

    updating resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,3 +1,55 @@
 import React from 'react'
+import '../styles/resume.css'
 
-export const resume = <p>My resume</p>
+export const resume =
+
+<div className='resume container'>
+  <div className='resume-goal'>
+    <h2>Goal</h2>
+    <p>To expand my knowledge of software development</p>
+  </div>
+  <div>
+    <h2>Projects</h2>
+    <ul>
+      <li>
+        <h3>Niyagapedia</h3>
+        <p>BlahBlah</p>
+      </li>
+      <li>
+        <h3>Another project</h3>
+      </li>
+      <li>
+        <h3>Another project</h3>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <h2>Education</h2>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+  <div>
+    <h2>Professional Background</h2>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+  </div>
+  <div>
+    <h2>Skills</h2>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+  </div>
+  <div>
+    <h2>Motivation</h2>
+    <p></p>
+  </div>
+</div>
